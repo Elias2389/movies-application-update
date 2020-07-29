0f4b3d79b5f6d7ba54588a8441d73a57dbf202c1
Andres <andres.rivas@mercadolibre.com>
2020-07-29 15:02:11 -0300

    Configuration of koin

--- app/src/main/java/com/ae/moviesapplicationupdate/base/MainApplication.kt
+++ app/src/main/java/com/ae/moviesapplicationupdate/base/MainApplication.kt
@@ -1,6 +1,22 @@
 package com.ae.moviesapplicationupdate.base
 
 import android.app.Application
+import com.ae.moviesapplicationupdate.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.fragment.koin.fragmentFactory
+import org.koin.core.context.startKoin
 
 class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            fragmentFactory()
+            androidLogger()
+            androidContext(this@MainApplication)
+
+            modules(appModule)
+        }
+    }
 }
