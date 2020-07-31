f025e1a71a9fbffc46300b06acbbac0f90729edd
Andres <andres.rivas@mercadolibre.com>
2020-07-31 15:07:47 -0300

    First views added

--- app/src/main/java/com/ae/moviesapplicationupdate/ui/movies/view/MoviesFragment.kt
+++ app/src/main/java/com/ae/moviesapplicationupdate/ui/movies/view/MoviesFragment.kt
@@ -20,7 +20,6 @@ class MoviesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies, container, false)
 
     }
--- app/src/main/java/com/ae/moviesapplicationupdate/ui/movies/viewmodel/MoviesViewModel.kt
+++ app/src/main/java/com/ae/moviesapplicationupdate/ui/movies/viewmodel/MoviesViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.ae.moviesapplicationupdate.common.dto.MoviesResponse
+import com.ae.moviesapplicationupdate.common.dto.Resource
 import com.ae.moviesapplicationupdate.data.services.MoviesServices
 import kotlinx.coroutines.Dispatchers
 import retrofit2.Response
@@ -11,15 +12,14 @@ import retrofit2.Retrofit
 
 class MoviesViewModel(private val moviesServices: MoviesServices): ViewModel() {
 
-    val movies: LiveData<Response<MoviesResponse>> = liveData(Dispatchers.IO) {
+    val movies: LiveData<Resource<Response<MoviesResponse>>> = liveData(Dispatchers.IO) {
         val result = moviesServices.getPopularMovies()
-        //emit(Resource.loading(data = null))
+        emit(Resource.loading(data = null))
 
         try {
-            emit(result)
-            //emit(Resource.success(result))
+            emit(Resource.success(result))
         } catch (e: Exception) {
-            //emit(Resource.error(data = null, message = e.message ?: "Error Occurred!"))
+            emit(Resource.error(data = null, message = e.message ?: "Error Occurred!"))
         }
     }
 
