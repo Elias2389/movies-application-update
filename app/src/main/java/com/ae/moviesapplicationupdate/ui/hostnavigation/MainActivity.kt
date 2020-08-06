924e7c3b30a390e9dd680280770d86110164521e
Andres <andres.rivas@mercadolibre.com>
2020-08-06 17:20:38 -0300

    Added navigation

--- app/src/main/java/com/ae/moviesapplicationupdate/ui/hostnavigation/MainActivity.kt
+++ app/src/main/java/com/ae/moviesapplicationupdate/ui/hostnavigation/MainActivity.kt
@@ -2,19 +2,31 @@ package com.ae.moviesapplicationupdate.ui.hostnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
+
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.ae.moviesapplicationupdate.R
-import com.ae.moviesapplicationupdate.ui.movies.viewmodel.MoviesViewModel
-import org.koin.android.ext.android.inject
-import org.koin.androidx.fragment.android.setupKoinFragmentFactory
+import com.ae.moviesapplicationupdate.databinding.ActivityMainBinding
+
+
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
     }
+
+
 }
